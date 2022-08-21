c5c4e50ace75a9ad79e89218ef880d649323741e
Webkueche <107237932+Webkueche@users.noreply.github.com>
2022-08-21 12:26:13 +0200

    Add files via upload

---
+++ Eieruhr/js/timer.js
@@ -0,0 +1,25 @@
+let alarm = new Audio('sounds/alarm.wav');
+
+
+function startTimer() {
+    let startTime = new Date().getTime();
+    let fiveMinutes = 1000 * 60 * 5;
+    let endTime = startTime + fiveMinutes;
+
+    setInterval(function() {
+        let timeLeft = endTime - new Date().getTime();
+
+
+        if (timeLeft > 0){
+        let minutes = timeLeft / (1000 * 60);
+        minutes = Math.floor(minutes);
+        let seconds = (timeLeft / 1000) % 60;
+        seconds = Math.round(seconds);
+        let text = '0' + minutes + ' : ' + seconds;
+        timer.innerHTML = text;
+    } else {
+        timer.innerHTML = '00:00';
+        alarm.play();
+    }
+    },1000);
+}
